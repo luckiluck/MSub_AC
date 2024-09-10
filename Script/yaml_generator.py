3ac10b4c8755e36d9d4324019f5ba32f4f756d0c
luckiluck <thucson147@gmail.com>
2024-09-10 14:51:08 +0900

    Update yaml generator script to obtain ID from Anime Kitsu
    Signed-off-by: luckiluck <thucson147@gmail.com>

--- Script/yaml_generator.py
+++ Script/yaml_generator.py
@@ -3,6 +3,8 @@ import re
 import yaml
 from urllib.parse import quote, urljoin
 
+from get_kitsu_id import get_kitsu_id
+
 def extract_season_episode(filename):
     match = re.search(r'S(\d+)E(\d+)', filename)
     if match:
@@ -18,7 +20,7 @@ def url_encode_path(path):
     path = path.replace('\\', '/')
     return quote(path)
 
-def generate_yaml(base_directory, base_url):
+def generate_yaml(base_directory, base_url, kitsu_id=True):
     seasons = {}
 
     encoded_base_dir = url_encode_path(os.path.basename(base_directory))
@@ -29,20 +31,25 @@ def generate_yaml(base_directory, base_url):
 
         if filename.endswith('.srt'):
             season, episode = extract_season_episode(filename)
+            if 'Unknown' not in seasons:
+                seasons['Unknown'] = []
+
+            # Use os.path.join for the relative path, then replace backslashes
+            encoded_path = url_encode_path(filename)
+
             if episode is not None:
-                if 'Unknown' not in seasons:
-                    seasons['Unknown'] = []
-
-                # Use os.path.join for the relative path, then replace backslashes
-                encoded_path = url_encode_path(filename)
-
                 seasons['Unknown'].append({
                     'path': encoded_path,
                     'episode': episode,
                     #'delay': -1.00  # Default delay, you can adjust this as needed
                 })
             else:
-                print(f"Skipping file: {filename}")
+                print(f"\033[95mMovie type file: {filename} or episode number not found.\033[0m\n")
+                seasons['Unknown'].append({
+                    'path': encoded_path,
+                    'episode': 1,
+                    #'delay': -1.00  # Default delay, you can adjust this as needed
+                })
 
             yaml_data = {
                 'msubtitles': [{
@@ -57,7 +64,13 @@ def generate_yaml(base_directory, base_url):
             }
 
     if yaml_data:
-        print(base_url + encoded_base_dir + '/' + f'{encoded_base_dir}.yaml')
+        print('Link to generated YAML file:')
+        print(base_url + encoded_base_dir + '/' + f'{encoded_base_dir}.yaml\n')
+
+        # Get Kitsu ID
+        if kitsu_id:
+            anime_id = get_kitsu_id(base_directory)
+            yaml_data['msubtitles'][0]['subtitles'][0]['season'] = int(anime_id)
 
         with open(f'{base_directory + "/" + os.path.basename(base_directory)}.yaml', 'w', encoding='utf-8') as f:
             f.write(yaml.dump(yaml_data, sort_keys=False, allow_unicode=True))
@@ -69,8 +82,6 @@ def generate_yaml(base_directory, base_url):
         season_path = os.path.join(base_directory, season_dir)
 
         if os.path.isdir(season_path):
-
-            print(base_url + encoded_base_dir + '/' + url_encode_path(season_dir + '/') + quote(f"{season_dir}.yaml"))
 
             for filename in os.listdir(season_path):
                 if filename.endswith('.srt'):
@@ -92,26 +103,35 @@ def generate_yaml(base_directory, base_url):
                         else:
                             print(f"Skipping file: {filename}")
 
-            yaml_data = {
-                'msubtitles': [{
-                    'source': full_base_url,
-                    'subtitles': [
-                        {
-                            'season': season,
-                            'items': sorted(items, key=lambda x: x['episode'])
-                        } for season, items in sorted(seasons.items())
-                    ]
-                }]
-            }
+            if seasons:
+                print('Link to generated YAML file:')
+                print(base_url + encoded_base_dir + '/' + url_encode_path(season_dir + '/') + quote(f"{season_dir}.yaml"))
 
-            with open(f'{base_directory + "/" + season_dir + "/" + season_dir}.yaml', 'w', encoding='utf-8') as f:
-                f.write(yaml.dump(yaml_data, sort_keys=False, allow_unicode=True))
+                # Get Kitsu ID
+                anime_id = None
+                if kitsu_id:
+                    anime_id = get_kitsu_id(base_directory)
+
+                yaml_data = {
+                    'msubtitles': [{
+                        'source': full_base_url,
+                        'subtitles': [
+                            {
+                                'season': int(anime_id) if anime_id is not None else season,
+                                'items': sorted(items, key=lambda x: x['episode'])
+                            } for season, items in sorted(seasons.items())
+                        ]
+                    }]
+                }
+
+                with open(f'{base_directory + "/" + season_dir + "/" + season_dir}.yaml', 'w', encoding='utf-8') as f:
+                    f.write(yaml.dump(yaml_data, sort_keys=False, allow_unicode=True))
 
     return
 
 
 # Example usage
-base_directory = 'C:\AC\Working on\Github\MSub_AC\Japanese_Subtitles\Higurashi no Naku Koro ni Kai'  # Replace with your actual base directory path
+base_directory = 'C:\AC\Working on\Github\MSub_AC\Japanese_Subtitles\Hyouka 1-22+OAD'  # Replace with your actual base directory path
 base_url = 'https://raw.githubusercontent.com/luckiluck/MSub_AC/main/Japanese_Subtitles/'  # Replace with your actual base URL
 yaml_content = generate_yaml(base_directory, base_url)
 
